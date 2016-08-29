ce1fc39d673923b4c8cd2bfd78d1e00a46773a73
Arthur Adams <arthuradams@gmail.com>
2016-08-29 00:07:08 -0400

    start of portfolio page

--- aca.js
+++ aca.js
@@ -2,20 +2,15 @@ $(document).ready(function(){
 
     var prev_id, id, filename;
 
+    $("#intro").addClass( "active" );
     $("#core").load("intro.html");
 
     $(".menuitem").on("click", function(){
-
           prev_id = id;
-
           $("#".concat(prev_id)).removeClass( "active" );
-
           id = this.id;
-
           $("#".concat(id)).addClass( "active" );
-
           filename = id.concat(".html");
-
           $("#core").load(filename);
         }
       )
